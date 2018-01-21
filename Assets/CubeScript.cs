abef3e6fd7ad3e08b0242bf93220dacc495bac68
Piotr Penar <penarpiotr27@gmail.com>
2018-01-21 13:15:45 +0100

    Initial Comit

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+	
+	void Update () {
+		if (Input.touchCount > 0) 
+		{
+			Touch touch = Input.GetTouch (0);
+			if (touch.phase == TouchPhase.Began) 
+			{
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				if (Physics.Raycast (ray)) 
+				{
+					FindObjectOfType<GameManager> ().AddScore ();
+					Destroy (gameObject);
+				}
+			}
+		}
+		if (transform.position.y < -10) 
+		{
+			FindObjectOfType<GameManager> ().EndGame ();
+		}
+	}
+
+}
